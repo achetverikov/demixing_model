281fccb5335038cdd924f1e34b22005855b4c5de
Andrey Chetverikov <achetverikov@users.noreply.github.com>
2026-09-25 11:37:21 +0200

    Keep search flag on fitter only

--- tests/run_smoke_pipeline.sh
+++ tests/run_smoke_pipeline.sh
@@ -84,7 +84,6 @@ $PYTHON_BIN model_fit_to_data/fit_model_to_data.py \
 $PYTHON_BIN model_fit_to_data/create_unified_subject_plots.py \
   --results-path "model_fit_to_data_smoke_pipeline/extended_fit_results.pkl" \
   --checkpoint-path "checkpoints_smoke_pipeline/model_epoch_0025.pkl" \
-  --search hierarchical \
   --output-dir "model_fit_to_data_smoke_pipeline" \
   --individual-plots \
   --summary-plots
