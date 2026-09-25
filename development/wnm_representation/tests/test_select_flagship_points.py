e9a19f35d85855d157de6ceb22ab2d3874abc220
Andrey Chetverikov <achetverikov@users.noreply.github.com>
2026-09-25 19:02:26 +0200

    refactor: move development-only WNM test

---
+++ development/wnm_representation/tests/test_select_flagship_points.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+from development.wnm_representation import select_flagship_points as flagships
+
+
+def test_candidate_point_score_uses_worst_normalized_metric():
+    frame = pd.DataFrame({
+        "raw_resultant": [0.9], "pred_mean_bias": [0.0], "raw_mean_bias": [0.0],
+        "pred_density_asymmetry": [0.006], "raw_density_asymmetry": [0.0],
+        "pred_response_sd": [10.0], "raw_response_sd": [10.0],
+        "circular_wasserstein_deg": [0.1],
+    })
+    np.testing.assert_allclose(flagships.candidate_point_scores(frame), [1.2])
