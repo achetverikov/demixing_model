bf61351ccd741dcd52a5ff2a5764ab4f6885b5d9
Andrey Chetverikov <achetverikov@users.noreply.github.com>
2026-09-25 13:01:58 +0200

    Fix legacy exhaustive test marker placement

--- tests/test_exhaustive_density.py
+++ tests/test_exhaustive_density.py
@@ -24,12 +24,12 @@ for path in (ROOT, ROOT / "model_fit_to_data"):
 
 import density_objective as do
 from exhaustive_density import (
-
-pytestmark = pytest.mark.legacy_surface
     InMemoryCurveSource,
     SUPPORTED_OBJECTIVES,
     fit_exhaustive_density,
 )
+
+pytestmark = pytest.mark.legacy_surface
 
 N_POINTS = 30
 
